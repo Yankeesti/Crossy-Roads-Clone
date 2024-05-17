b25e0c1be699dba9cc39ee91f8237678fc4e63f3
Tim Heinsberg <tim.heinsberg@smail.th-koeln.de>
2024-05-17 23:16:08 +0200

    implemented way to print input only when wanted

--- src/human-player.py
+++ src/human-player.py
@@ -3,10 +3,10 @@ import itertools
 import pygame
 
 navigation_action = "stay"
-
+print_input = False
 
 def handle_key_press():
-    global navigation_action
+    global navigation_action, print_input
     out_put = {"run": True}
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,12 +20,17 @@ def handle_key_press():
                 navigation_action = "up"
             if event.key == pygame.K_s:
                 navigation_action = "down"
+            if event.key == pygame.K_p:
+                print_input = True
     return out_put
 
 
 class HumanController:
+    def __init__(self) -> None:
+        self.moves_executet = {"left": 0, "right": 0, "up": 0, "down": 0, "stay": 0}
+
     def get_action(self, input):
-        global navigation_action
+        global navigation_action,print_input
         out_put = navigation_action
         navigation_action = "stay"
         flat_list = list(
@@ -38,7 +43,9 @@ class HumanController:
                 (i if isinstance(i, tuple) else [i] for i in flat_list)
             )
         )
-        print(input)
+        if print_input:
+            print(input)
+            print_input = False
         return out_put
 
     def setFitness(self, fitness):
@@ -57,12 +64,3 @@ while True:
         break
     camera.draw()
 
-gameClass.reset(controllers=[controller])
-camera = game.Camera()
-camera.set_player_to_draw(gameClass.playerManager.players[0])
-camera.draw()
-while True:
-    clock.tick(60)
-    if handle_key_press() == False or gameClass.update() == False:
-        break
-    camera.draw()
